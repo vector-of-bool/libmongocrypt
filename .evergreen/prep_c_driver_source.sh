ed47d63e04eb2623c17ae134d50b74ce03cdd838
vector-of-bool <vectorofbool@gmail.com>
2022-02-28 04:04:55 +0000

    Update prep_c_driver_source

--- .evergreen/prep_c_driver_source.sh
+++ .evergreen/prep_c_driver_source.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-set -o xtrace
-set -o errexit
+set -e
+. "$(dirname "${BASH_SOURCE[0]}")/init.sh"
+
+# Change the path to the driver dir to a scratch-space clone
+MONGO_C_DRIVER_DIR="${BUILD_ROOT}/mcd-clone"
 
 # Clone mongo-c-driver and check out to a tagged version.
 MONGO_C_DRIVER_VERSION=1.17.0
 
 # Force checkout with lf endings since .sh must have lf, not crlf on Windows
-git clone git@github.com:mongodb/mongo-c-driver.git --config core.eol=lf --config core.autocrlf=false --depth=1 --branch $MONGO_C_DRIVER_VERSION
-echo $MONGO_C_DRIVER_VERSION > mongo-c-driver/VERSION_CURRENT
-sed -i.orig -E -e 's/(elif )(command -v cmake 2>\/dev\/null; then)/\1[ -z "\$IGNORE_SYSTEM_CMAKE" ] \&\& \2/' mongo-c-driver/.evergreen/find-cmake.sh && rm -f mongo-c-driver/.evergreen/find-cmake.sh.orig
+if ! test -d "${MONGO_C_DRIVER_DIR}/.git"; then
+    echo "Cloning mongo-c-driver@${MONGO_C_DRIVER_VERSION}"
+    git clone https://github.com/mongodb/mongo-c-driver.git \
+        --config core.eol=lf \
+        --config core.autocrlf=false \
+        --config advice.detachedHead=false \
+        --quiet \
+        --depth=1 \
+        --branch="$MONGO_C_DRIVER_VERSION" \
+        "${MONGO_C_DRIVER_DIR}"
+fi
+git -C "${MONGO_C_DRIVER_DIR}" fetch origin "${MONGO_C_DRIVER_VERSION}" --depth=1
+git -C "${MONGO_C_DRIVER_DIR}" checkout "${MONGO_C_DRIVER_VERSION}"
+
+echo $MONGO_C_DRIVER_VERSION > "${MONGO_C_DRIVER_DIR}/VERSION_CURRENT"
+
+# Override the project's find-cmake to use the same one that we are using
+echo "CMAKE=\"$(get_cmake_exe)\"" > "${MONGO_C_DRIVER_DIR}/.evergreen/find-cmake.sh"
