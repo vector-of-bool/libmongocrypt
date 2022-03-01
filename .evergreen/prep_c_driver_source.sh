498c81fb758bfbd57661bfdabe2ec478b257ed57
vector-of-bool <vectorofbool@gmail.com>
2022-03-01 03:20:51 +0000

    Make parent dir before cloning

--- .evergreen/prep_c_driver_source.sh
+++ .evergreen/prep_c_driver_source.sh
@@ -6,6 +6,7 @@ set -e
 # Clone mongo-c-driver and check out to a tagged version.
 MONGO_C_DRIVER_VERSION=1.17.0
 
+mkdir -p "${MONGO_C_DRIVER_DIR}"
 # Force checkout with lf endings since .sh must have lf, not crlf on Windows
 if ! test -d "${MONGO_C_DRIVER_DIR}/.git"; then
     log "Cloning mongo-c-driver@${MONGO_C_DRIVER_VERSION}"
@@ -21,11 +22,10 @@ fi
 git -C "${MONGO_C_DRIVER_DIR}" fetch origin "${MONGO_C_DRIVER_VERSION}" --depth=1
 git -C "${MONGO_C_DRIVER_DIR}" checkout "${MONGO_C_DRIVER_VERSION}"
 
-_mcd_dir="$(native_path "${MONGO_C_DRIVER_DIR}")"
-echo $MONGO_C_DRIVER_VERSION > ${_mcd_dir}/VERSION_CURRENT
+log "mongo-c-driver fetched into [${MONGO_C_DRIVER_DIR}]"
 
-log "mongo-c-driver fetched into [${_mcd_dir}]"
+echo $MONGO_C_DRIVER_VERSION > ${MONGO_C_DRIVER_DIR}/VERSION_CURRENT
 
 # Override the project's find-cmake to use the same one that we are using
 _cmake="$(get_cmake_exe)"
-echo "CMAKE=\"$_cmake\"" > ${_mcd_dir}/.evergreen/find-cmake.sh
+echo "CMAKE=\"$_cmake\"" > ${MONGO_C_DRIVER_DIR}/.evergreen/find-cmake.sh
