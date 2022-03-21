71a0d294e0d32bc375505250146f68e16ee3dd6b
vector-of-bool <vectorofbool@gmail.com>
2022-03-21 16:57:50 -0600

    Fix MLIB_MAP for MSVC

--- src/mlib/macros.h
+++ src/mlib/macros.h
@@ -11,22 +11,36 @@
 
 #define MLIB_EMPTY
 
+
+#ifdef __VA_OPT__
+#define pMLIB_FIRST(arg, ...) arg
+#define MLIB_IS_EMPTY(...) pMLIB_FIRST(__VA_OPT__(0, ), 1)
+#else
+#define pMLIB_PICK_32ND(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32) _32
+#define pMLIB_HAS_COMMA(...) pMLIB_PICK_32ND(__VA_ARGS__, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0)
+#define pMLIB_COMMA_CALL(...) ,
+#define pMLIB_PASTE5(a, b, c, d, e) a##b##c##d##e
+#define pMLIB_IS_EMPTY_0001 ,
+#define pMLIB_IS_EMPTY(HasComma, HasParens, IsFnMacro, IsEmpty) \
+   pMLIB_HAS_COMMA(pMLIB_PASTE5(pMLIB_IS_EMPTY_, Hascomma, HasParents, IsFnMacro, IsEmpty))
+#define MLIB_IS_EMPTY(...) \
+   pMLIB_IS_EMPTY(pMLIB_HAS_COMMA(__VA_ARGS__), pMLIB_HAS_COMMA(pMLIB_COMMA_CALL __VA_ARGS__), pMLIB_HAS_COMMA(__VA_ARGS__ ()), pMLIB_HAS_COMMA(pMLIB_COMMA_CALL __VA_ARGS__ ()))
+#endif
+
 #define pMLIB_MAP_END(...)
-#define pMLIB_MAP_GET_END(...) 0, pMLIB_MAP_END
 
-#define pMLIB_MAP_NEXT_2(Elem, Next, ...) Next MLIB_EMPTY
-#define pMLIB_MAP_NEXT_1(Elem, Next) pMLIB_MAP_NEXT_2 (Elem, Next, 0)
-#define pMLIB_MAP_NEXT(Elem, Next) \
-   pMLIB_MAP_NEXT_1 (pMLIB_MAP_GET_END Elem, Next)
+#define pMLIB_MAP_NEXT_PICK_0(Next) Next
+#define pMLIB_MAP_NEXT_PICK_1(Next) pMLIB_MAP_END
+#define pMLIB_MAP_NEXT(IsEmpty, Next) MLIB_CONCAT(pMLIB_MAP_NEXT_PICK_, IsEmpty)(Next)
 
-#define pMLIB_MAP_B(Do, Constnat, Counter, Element, Next, ...) \
+#define pMLIB_MAP_B(Do, Constnat, Counter, Element,  ...) \
    Do (Constant, Counter, Element) /* */                       \
-      pMLIB_MAP_NEXT (Next, pMLIB_MAP_A) (                     \
-         Do, Constant, Counter + 1, Next, __VA_ARGS__)
-#define pMLIB_MAP_A(Do, Constnat, Counter, Element, Next, ...) \
+      pMLIB_MAP_NEXT (MLIB_IS_EMPTY(__VA_ARGS__), pMLIB_MAP_A) (                     \
+         Do, Constant, Counter + 1, __VA_ARGS__)
+#define pMLIB_MAP_A(Do, Constnat, Counter, Element, ...) \
    Do (Constant, Counter, Element) /* */                       \
-      pMLIB_MAP_NEXT (Next, pMLIB_MAP_B) (                     \
-         Do, Constant, Counter + 1, Next, __VA_ARGS__)
+      pMLIB_MAP_NEXT (MLIB_IS_EMPTY(__VA_ARGS__), pMLIB_MAP_B) (                     \
+         Do, Constant, Counter + 1, __VA_ARGS__)
 
 #define pMLIB_MAP_1(Do, Constant, ...) \
    MLIB_EVAL (pMLIB_MAP_A (Do, Constant, 0, __VA_ARGS__, (), 0))
