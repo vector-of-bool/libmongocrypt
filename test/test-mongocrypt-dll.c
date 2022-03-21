f9f875a3fd3884f717e9f91a0bebe8a9baee0ba6
vector-of-bool <vectorofbool@gmail.com>
2022-03-21 22:55:47 +0000

    Update dll test

--- test/test-mongocrypt-dll.c
+++ test/test-mongocrypt-dll.c
@@ -11,16 +11,19 @@ _test_load_simple_library (_mongocrypt_tester_t *t)
 {
    (void) t;
    mstr self_path = mstr_copy_cstr (TEST_MONGOCRYPT_OUTPUT_PATH);
-
    mstr dll_path = mpath_join (mpath_parent (self_path.view, MPATH_NATIVE),
                                mstrv_view_cstr ("test-dll.dll"),
                                MPATH_NATIVE);
+   mstr_free (self_path);
 
    mcr_dll lib;
-   MERROR_HANDLER_BLOCK (dll_open_error)
    {
+      merror_handler_push (dll_open_error);
       lib = mcr_dll_open (dll_path.data);
-      BSON_ASSERT (!e_dll_open_error.got_error);
+      merror_handler_deactivate ();
+      BSON_ASSERT (lib.error.id == 0);
+      BSON_ASSERT (!merror_get (lib.error, dll_open_error));
+      merror_handler_pop ();
    }
 
    int (*say_hello) (void) = mcr_dll_sym (lib, "say_hello");
@@ -30,9 +33,17 @@ _test_load_simple_library (_mongocrypt_tester_t *t)
    ASSERT_CMPINT (rval, ==, 42);
 
    mcr_dll_close (lib);
+
+   MERROR_HANDLER_BLOCK (dll_open_error)
+   {
+      lib = mcr_dll_open (dll_path.data);
+      merror_handler_deactivate ();
+      BSON_ASSERT (lib.error.id == 0);
+      BSON_ASSERT (!merror_get (lib.error, dll_open_error));
+      break;
+   }
+
    mstr_free (dll_path);
-   mstr_free (self_path);
-   BSON_ASSERT (!merror_has_handler (dll_open_error));
 }
 
 static void
@@ -42,8 +53,9 @@ _test_load_nonesuch (_mongocrypt_tester_t *t)
    MERROR_HANDLER_BLOCK (dll_open_error)
    {
       mcr_dll lib = mcr_dll_open ("no-such-directory/no-such-lib.dll");
+      merror_handler_deactivate ();
       BSON_ASSERT (lib._native_handle == NULL);
-      BSON_ASSERT (e_dll_open_error.got_error);
+      BSON_ASSERT (merror_get (lib.error, dll_open_error));
       mcr_dll_close (lib);
    }
 }
