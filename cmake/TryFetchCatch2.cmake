cfbc3992539473f01706a0a108c1d14ac63c3cf4
vector-of-bool <vectorofbool@gmail.com>
2022-11-28 22:02:48 +0000

    Revert "Lower the Catch2 version for better platform support"
    This reverts commit a710c1a28929df0b1722b1a9d10397cc359cd20c.

--- cmake/TryFetchCatch2.cmake
+++ cmake/TryFetchCatch2.cmake
@@ -16,36 +16,30 @@ set (required_features cxx_alignas cxx_alignof cxx_attributes cxx_auto_type
     cxx_constexpr cxx_defaulted_functions cxx_deleted_functions cxx_final
     cxx_lambdas cxx_noexcept cxx_override cxx_range_for cxx_rvalue_references
     cxx_static_assert cxx_strong_enums cxx_trailing_return_types
-    cxx_unicode_literals cxx_user_literals cxx_variadic_macros
-    cxx_return_type_deduction)
+    cxx_unicode_literals cxx_user_literals cxx_variable_templates
+    cxx_variadic_macros)
 
-set (missing_features ${required_features})
-list (REMOVE_ITEM missing_features ${CMAKE_CXX_COMPILE_FEATURES} ~~~ ~~~)
+list (REMOVE_ITEM required_features ${CMAKE_CXX_COMPILE_FEATURES})
 
-if (missing_features)
-    string (REPLACE ";" ", " missing "${missing_features}")
+if (required_features)
+    string (REPLACE ";" ", " missing "${required_features}")
     message (STATUS "Not using Catch2: C++ compiler is missing required features: ${missing}")
     return ()
 endif ()
 
-# Obtain Catch2 v2.13.10
+# Obtain Catch2 v3.1.0
 FetchContent_Declare (Catch2
-    URL "https://github.com/catchorg/Catch2/archive/refs/tags/v2.13.10.zip"
+    URL "https://github.com/catchorg/Catch2/archive/refs/tags/v3.1.0.zip"
     )
 
 FetchContent_Populate (Catch2)
 
-add_library (catch2-interface INTERFACE)
-add_library (Catch2::Catch2 ALIAS catch2-interface)
-target_include_directories (catch2-interface INTERFACE "${catch2_SOURCE_DIR}/single_include")
-target_compile_features (catch2-interface INTERFACE ${required_features})
-
-# # Build a library from the amalgamation. This builds significantly faster than
-# # compiling all files individually (we don't care about incremental compilation)
-add_library (catch2-main STATIC "${CMAKE_CURRENT_LIST_DIR}/catch2-main.cpp")
-add_library (Catch2::Main ALIAS catch2-main)
-target_link_libraries (catch2-main PUBLIC catch2-interface)
-
 # Import the module, used to define/discover tests automatically
-list (APPEND CMAKE_MODULE_PATH "${catch2_SOURCE_DIR}/contrib")
+list (APPEND CMAKE_MODULE_PATH "${catch2_SOURCE_DIR}/extras")
 include (Catch)
+
+# Build a library from the amalgamation. This builds significantly faster than
+# compiling all files individually (we don't care about incremental compilation)
+add_library (Catch2WithMain STATIC "${catch2_SOURCE_DIR}/extras/catch_amalgamated.cpp")
+target_include_directories (Catch2WithMain INTERFACE "${catch2_SOURCE_DIR}/extras")
+
--- src/mc-dec128.test.cpp
+++ src/mc-dec128.test.cpp
@@ -1,4 +1,4 @@
-#include <catch2/catch.hpp>
+#include <catch_amalgamated.hpp>
 
 #include "./mc-dec128.h"
 
